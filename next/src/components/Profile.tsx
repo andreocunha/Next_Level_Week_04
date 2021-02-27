7e9fa6404d199d53f7d1394c817cdf48ae1268f9
andreocunha <andreoliveiracunha20@gmail.com>
2021-02-26 21:17:38 -0300

    Dia 4

--- next/src/components/Profile.tsx
+++ next/src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(){
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/andreocunha.png" alt="André Cunha"/>
@@ -8,7 +11,7 @@ export function Profile(){
                 <strong>André Cunha</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
